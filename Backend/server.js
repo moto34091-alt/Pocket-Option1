ffd953be5df2c5c416b0e5ff3f547c2796373dc7
Support Vimba boss <moto34091@gmail.com>
2026-05-27 16:00:05 +0200

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,29 +1,13 @@
-require("dotenv").config();
-
 const express = require("express");
-const cors = require("cors");
 
 const app = express();
 
-app.use(cors());
-app.use(express.json());
-
 app.get("/", (req, res) => {
-  res.json({
-    status: "OTC Trading AI Running"
-  });
-});
-
-app.get("/signals/eurusd", (req, res) => {
-  res.json({
-    pair: "EUR/USD OTC",
-    signal: "BUY",
-    confidence: 87
-  });
+  res.send("Railway Working");
 });
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server running on ${PORT}`);
+  console.log("Server running");
 });
